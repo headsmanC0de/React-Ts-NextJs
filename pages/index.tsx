7e53d9b10d2c546ba364b2e6438b37a128af7e0c
headsmanc0de <headsmanc0de@gmail.com>
2022-02-03 00:19:45 +0200

    Add Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { Button, Htag } from '../src/components';
+import { Button, Htag, P, Tag } from '../src/components';
 
 const Home: NextPage = (): JSX.Element => {
 
@@ -7,7 +7,17 @@ const Home: NextPage = (): JSX.Element => {
     <>
       <Htag tag='h3'>dude</Htag>
       <Button appearance='primary' arrow='right'>Button</Button>
-      <Button appearance='ghost' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
+      <Button appearance='ghost'>Button</Button>
+      <P size='l'>Large</P>
+      <P size='m'>Medium</P>
+      <P size='s'>Small</P>
+      <Tag size='s'>Small Tag</Tag>
+      <Tag size='m' color='ghost'>Some text</Tag>
+      <Tag size='m' color='red'>Some text</Tag>
+      <Tag size='m' color='grey'>Some text</Tag>
+      <Tag size='m' color='green'>Some text</Tag>
+      <Tag size='m' color='primary'>Some text</Tag>
     </>
   );
 };
